d9b0ed0f21219365d954d82074fea1e3629905c2
Hiago P. Inácio <hiagopinacio@gmail.com>
2023-12-08 17:11:52 -0300

    todo curso

---
+++ src/leilao/principal.py
@@ -0,0 +1,16 @@
+from dominio import Usuario, Lance, Leilao
+
+gui = Usuario("Gui")
+yuri = Usuario("Yuri")
+
+lance_do_yuri = Lance(yuri, 100.0)
+lance_do_gui = Lance(gui, 150.0)
+
+leilao = Leilao("Celular")
+
+leilao.lances.append(lance_do_yuri)
+leilao.lances.append(lance_do_gui)
+
+for lance in leilao.lances:
+    print(f"O usuario {lance.usuario.nome} deu um lance de {lance.valor}")
+
